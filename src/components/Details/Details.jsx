bfb10190d3382ffa3a21aca26430c6289afff295
Joshua McCollum <xmtuner629@yahoo.com>
2021-12-12 00:57:36 -0600

    added details to page using store

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux'; 
 
 function Details( props ){
-    // const reducerName = useSelector( store => store.reducerName );
+   const details = useSelector( store => store.details );
     // const [name, setName] = useState( null );
 
     const dispatch = useDispatch();
@@ -10,6 +10,18 @@ function Details( props ){
     return(
         <div>
             <h1>Details</h1>
+            <img src={details[0].poster} alt={details[0].title}/>
+
+            <p>Name: {details[0].title}</p>
+            <p>Description: {details[0].description}</p>
+            <p>Genre: {details.map(detail =>{
+                return (
+                    <div key = {detail.id}>
+                    <li>{detail.name}</li>
+                    </div>
+                )
+            })}</p>
+
         </div>
     )
 }
